0dad0cfb57e145e63d006f09d27dd6ef05bc9f1c
Opposolis <wmsjshihao@gmail.com>
2024-08-30 11:56:49 +0800

    add 2

--- 代码随想录/图论/城市间货物运输/test.cpp
+++ 代码随想录/图论/城市间货物运输/test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <limits>
+#include <list>
+#include <queue>
 #include <vector>
 
 struct Edge{
-    int u, v, weight;
+    int to;
+    int weight;
+    Edge(int t, int w): to(t), weight(w) {}
 };
+
 
 const int INF = std::numeric_limits<int>::max();
 
@@ -12,35 +17,39 @@ int main()
 {
     int n=0, m=0;
     std::cin>>n>>m;
-    std::vector<Edge> edges(m);
+    std::vector<std::list<Edge>> adjList(n+1);
+    int from=0, to=0, weight=0;
     for(int i=0; i<m; ++i)
     {
-        std::cin>>edges[i].u>>edges[i].v>>edges[i].weight;
+        std::cin>>from>>to>>weight;
+        adjList[from].emplace_back(to, weight);
     }
 
     std::vector<int> dist(n+1, INF);
-
+    std::vector<bool> visited(n+1, false);
+    std::queue<int> que;
     int start = 1, end = n;
+    
+    que.push(start);
     dist[start]=0;
-    // V-1 次松弛
-    for(int i=1; i<n; i++)
+
+    while(!que.empty())
     {
-        for(const Edge& e: edges)
+        int cur = que.front();
+        que.pop();
+        visited[cur] = false;   // 出队列，变为未访问
+        // 遍历当前节点的邻接节点
+        for(const Edge& e: adjList[cur])
         {
-            if (dist[e.u]!=INF && dist[e.v]>dist[e.u]+e.weight) {
-                dist[e.v] = dist[e.u] + e.weight;
+            if(dist[e.to] > dist[cur] + e.weight)
+            {
+                dist[e.to] = dist[cur] + e.weight;
+                if(!visited[e.to])
+                    que.push(e.to);
             }
         }
     }
-    // 第V次进行检查负环
-    for(const Edge& e: edges)
-    {
-        if (dist[e.u]!=INF && dist[e.v]>dist[e.u]+e.weight) {
-            std::cerr<<"负环";
-            return 1;
-        }
-    }
-
+    
     if (dist[end]==INF) {
         std::cout<<"unconnected";
     } else {
---
+++ 代码随想录/图论/城市间货物运输/test2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+
+struct Edge{
+    int u, v, weight;
+};
+
+const int INF = std::numeric_limits<int>::max();
+
+int main()
+{
+    int n=0, m=0;
+    std::cin>>n>>m;
+    std::vector<Edge> edges(m);
+    for(int i=0; i<m; ++i)
+    {
+        std::cin>>edges[i].u>>edges[i].v>>edges[i].weight;
+    }
+
+    std::vector<int> dist(n+1, INF);
+
+    int start = 1, end = n;
+    dist[start]=0;
+    // V-1 次松弛
+    for(int i=1; i<n; i++)
+    {
+        for(const Edge& e: edges)
+        {
+            if (dist[e.u]!=INF && dist[e.v]>dist[e.u]+e.weight) {
+                dist[e.v] = dist[e.u] + e.weight;
+            }
+        }
+    }
+    // 第V次进行检查负环
+    for(const Edge& e: edges)
+    {
+        if (dist[e.u]!=INF && dist[e.v]>dist[e.u]+e.weight) {
+            std::cerr<<"负环";
+            return 1;
+        }
+    }
+
+    if (dist[end]==INF) {
+        std::cout<<"unconnected";
+    } else {
+        std::cout<<dist[n];
+    }
+    return 0;
+}
+
