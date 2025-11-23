f0ee9d886ba2276c4c798755fc1683e1dd74f0f4
desktop <3037194892@qq.com>
2025-11-24 00:05:51 +0800

    add 1

---
+++ 剑指Offer（专项突破）/删除链表的倒数第 n 个结点/test.cpp
@@ -0,0 +1,31 @@
+#include "../../utils/utils.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        for (int i=0; i<n; ++i)
+        {
+            fast = fast->next;
+        }
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        ListNode* ret = dummy->next;
+        delete dummy;
+        return ret;
+    }
+};
